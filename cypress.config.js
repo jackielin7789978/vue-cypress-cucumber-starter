ca491bcda6d7ba7803695dcaeba3737570b0beb2
JackieLin <jackie.lin@easyuse.com.tw>
2022-10-13 17:16:04 +0800

    add eslint & prettier configs

--- cypress.config.js
+++ cypress.config.js
@@ -1,29 +1,29 @@
 /* eslint-disable no-undef */
-const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const { defineConfig } = require('cypress')
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+const preprocessor = require('@badeball/cypress-cucumber-preprocessor')
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild')
 
 async function setupNodeEvents(on, config) {
-  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  await preprocessor.addCucumberPreprocessorPlugin(on, config)
 
   on(
-    "file:preprocessor",
+    'file:preprocessor',
     createBundler({
-      plugins: [createEsbuildPlugin.default(config)],
+      plugins: [createEsbuildPlugin.default(config)]
     })
-  );
+  )
 
-  return config;
+  return config
 }
 
 module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 800,
   e2e: {
-    specPattern: "cypress/e2e/**/*.feature",
+    specPattern: 'cypress/e2e/**/*.feature',
     chromeWebSecurity: false,
-    baseUrl: "http://localhost:5173",
-    setupNodeEvents,
-  },
-});
+    baseUrl: 'http://localhost:5173',
+    setupNodeEvents
+  }
+})
